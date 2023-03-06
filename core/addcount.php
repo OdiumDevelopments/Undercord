adc412695daaee0a471f48227bccb0446175510d
Свисток <51795228+thewhistledev@users.noreply.github.com>
2023-03-06 00:08:17 +0000

    Update to false counting detection.
    - DetectFalseCount will now check to see if a minute has passed between counting the template as a legitimate user, this obviously has major flaws, as legit users will generate templates and some wont actually be counted, will be updated at a later date.

--- core/addcount.php
+++ core/addcount.php
@@ -15,7 +15,27 @@ function addCount(){
     file_put_contents($file, $jsonData);
 }
 
-function detectFalseCount(){
-    // Placeholder function
+function DetectFalseCount(){
+    $file = './core/data.json';
+    $data = json_decode(file_get_contents($file), true);
+
+    $lastCount = $data['count'];
+    $lastTime = $data['time'];
+
+    $currentTime = time();
+    $timeDiff = $currentTime - $lastTime;
+
+    if ($timeDiff > 60) { // Check if more than 1 minute has elapsed
+        $data['count'] = 0;
+        $data['time'] = $currentTime;
+        file_put_contents($file, json_encode($data));
+    } elseif ($lastCount >= 5) { // Check if more than 5 counts have occurred within 1 minute
+        // Spam detected, do nothing
+    } else {
+        $data['count'] = $lastCount + 1;
+        file_put_contents($file, json_encode($data));
+        AddCount();
+    }
 }
+
 ?>
