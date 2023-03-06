44c8fa4c1bf4dd30ab9745975f0c8ccce353ae5c
Свисток <51795228+thewhistledev@users.noreply.github.com>
2023-03-06 00:03:58 +0000

    Updated Counting Function to use JSON
    - addCount will now use JSON to validate and count data.
    - getCount will now use JSON to check the amount.
    - detectFalseCount is a placeholder function for the moment.

--- core/addcount.php
+++ core/addcount.php
@@ -1,20 +1,21 @@
 <?php
-function GetCount(){
-    $file = './core/generated.int'; 
-    $Number = file_get_contents($file); 
-    $fdata = intval($Number);
-    return $fdata;
-}
-//PPF6 -> Unable to open or read file (maybe doesnt exist or have permissions to read)
-function AddCount(){
-    $file = './core/generated.int'; 
-    $Number = file_get_contents($file); 
-    $fdata = intval($Number)+1; 
-    file_put_contents($file, $fdata); 
+function getCount(){
+    $file = './core/generated.json';
+    $data = file_get_contents($file);
+    $count = json_decode($data, true);
+    return $count['count'];
 }
 
-function DetectFalseCount(){
-    
+function addCount(){
+    $file = './core/generated.json';
+    $data = file_get_contents($file);
+    $count = json_decode($data, true);
+    $count['count']++;
+    $jsonData = json_encode($count);
+    file_put_contents($file, $jsonData);
 }
 
+function detectFalseCount(){
+    // Placeholder function
+}
 ?>
